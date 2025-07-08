57961e119afa803d5392b2549289f257303ae684
kyoko <kumakyon9s@gmail.com>
2025-07-08 15:28:44 +0900

    add:backにExpress + TypeScript初期構成を追加

---
+++ back/src/app.ts
@@ -0,0 +1,13 @@
+// src/app.ts
+import express from 'express';
+
+const app = express();
+const port = 4000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Express + TypeScript!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
