fe95560fb5f48bf5d2c909da24b251aa2841ed5d
kyoko <kumakyon9s@gmail.com>
2025-07-10 15:10:55 +0900

    feat:ログイン画面の修正・メール認証の実装・未認証ユーザーに対する404表示を追加

---
+++ front/src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+      <h1>404 - ページが見つかりません</h1>
+      <p>お探しのページは存在しないか、移動された可能性があります。</p>
+    </div>
+  );
+}
