7d222e7e9ca49cec8827e375b3c41b04e15f7d9d
kyoko <kumakyon9s@gmail.com>
2025-07-11 10:48:46 +0900

    add:Work ページのAPI接続機能の追加

--- back/src/app.ts
+++ back/src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import { PrismaClient } from '@prisma/client';
+import workRoutes from "./routes/work";
 
 const app = express();
 const prisma = new PrismaClient();
 
 app.use(express.json());
+app.use("/api/work", workRoutes);
 
 app.get('/profiles', async (req, res) => {
   const profiles = await prisma.profile.findMany();
@@ -14,4 +16,6 @@ app.get('/profiles', async (req, res) => {
 app.listen(4000, () => {
   console.log('Server running on http://localhost:4000');
 });
+
+export default app;
 
