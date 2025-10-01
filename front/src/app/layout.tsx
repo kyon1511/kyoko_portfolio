f4239b29ca1d8ef47fb538ab1c9e49ee754cb921
Kyoko Kumagai <kyon@KyokonoMacBook-Air.local>
2025-10-01 15:22:11 +0900

    chore: add baseUrl in tsconfig.json to enable alias (@) （エイリアス有効化のためbaseUrlを追加）

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -1,38 +1,13 @@
 import './globals.css';
-import Link from 'next/link';
 import { ReactNode } from 'react';
+import Navigation from '@/components/Navigation'; // ← ここで追加
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="ja">
       <body className="min-h-screen bg-[#c2cdff]">
-        {/* ナビゲーション（共通） */}
-        <nav className="w-full flex justify-center gap-40 p-6">
-          <Link
-            href="/works"
-            className="text-[#2b0934] font-bold hover:underline"
-          >
-            Works
-          </Link>
-          <Link
-            href="/skills"
-            className="text-[#2b0934] font-bold hover:underline"
-          >
-            Skill
-          </Link>
-          <Link
-            href="/about"
-            className="text-[#2b0934] font-bold hover:underline"
-          >
-            About
-          </Link>
-          <Link
-            href="/contact"
-            className="text-[#2b0934] font-bold hover:underline"
-          >
-            Contact
-          </Link>
-        </nav>
+        {/* 共通ナビ */}
+        <Navigation />
 
         {/* ページごとの中身 */}
         <main className="pt-24">{children}</main>
